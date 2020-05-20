cce71c27d63e9b83ab379dadc369d973a67cc3c9
Jeong Hyun Nam <jeonghyun.nam@devsisters.com>
2020-05-20 13:05:50 +0900

    Update generation script for non-Windows scenario and bug fix

--- generate_dockerfiles.ps1
+++ generate_dockerfiles.ps1
@@ -1,15 +1,52 @@
 $Utf8NoBomEncoding = New-Object System.Text.UTF8Encoding $False
 
-$TagList = ((Invoke-WebRequest -Uri "https://mcr.microsoft.com/v2/windows/servercore/tags/list" -UseBasicParsing).Content | ConvertFrom-Json).tags
+$TagList = ((Invoke-WebRequest -Uri "https://mcr.microsoft.com/v2/windows/servercore/tags/list").Content | ConvertFrom-Json).tags
 
 $PS_VERSION_LIST = '6.0.0','6.0.1','6.0.2','6.0.3','6.0.4','6.0.5','6.1.0','6.1.1','6.1.2','6.1.3','6.1.4','6.1.5','6.1.6','6.2.0','6.2.1','6.2.2','6.2.3','6.2.4','6.2.5','7.0.0','7.0.1'
 $OS_SKU_LIST = 'lstc2016','1607','1709','1803','ltsc2019','1809','1903','1909'
 
 foreach ($PS_VERSION in $PS_VERSION_LIST) {
     foreach ($OS_SKU in $OS_SKU_LIST) {
-        $VER_LIST = $ServerCoreTagList | Where-Object { ($_ -clike "${OS_SKU}-*" -or $_ -eq "${OS_SKU}") -and $_ -cnotlike "*-amd64" }
+        $VER_LIST = $TagList | Where-Object { ($_ -clike "${OS_SKU}-*" -or $_ -eq "${OS_SKU}") -and $_ -cnotlike "*-amd64" }
         foreach ($VER in $VER_LIST) {
-            $PowerShellCoreDockerfileTemplate = @"
+            $GitHubActionTemplate = @"
+name: PowerShell Core CI for ${PS_VERSION}-${VER}
+
+on:
+    push:
+        branches: [ master ]
+    pull_request:
+        branches: [ master ]
+
+jobs:
+
+    build-pwsh:
+        runs-on: [self-hosted, windows, x64]
+        env:
+            working-directory: .\pwsh\windows
+
+        steps:
+            - name: Checkout
+              uses: actions/checkout@v2.1.0
+            
+            - name: Docker Login
+              uses: Azure/docker-login@v1
+              with:
+                username: rkttu
+                password: `${{ secrets.GITHUBKEY }}
+                login-server: https://index.docker.io/v1/
+
+            - name: Build the PowerShell Core (${PS_VERISON}-${VER})
+              shell: powershell
+              env:
+                tag: ${PS_VERSION}-nanoserver-${VER}
+              working-directory: `${{env.working-directory}}/`${{env.tag}}
+              run: |
+                docker build . --isolation=hyperv --tag `"rkttu/pwsh:`${env:tag}`";
+                docker push `"rkttu/pwsh:`${env:tag}`";
+"@;
+
+            $DockerfileTemplate = @"
 # escape=``
 
 FROM mcr.microsoft.com/windows/servercore:${VER}  AS installer-env
@@ -60,9 +97,13 @@ ENV PATH="`$PATH;`$ProgramFiles\PowerShell"
 
 CMD ["pwsh.exe"]
 "@;
-            $TARGET_PATH = ".\pwsh\windows\${PS_VERSION}-nanoserver-${VER}"
-            mkdir -Path $TARGET_PATH -Force
-            [System.IO.File]::WriteAllLines("${TARGET_PATH}\Dockerfile", $PowerShellCoreDockerfileTemplate, $Utf8NoBomEncoding)
+            $TARGET_PATH = "./.github/workflows"
+            New-Item -Type Directory -Path $TARGET_PATH -Force
+            [System.IO.File]::WriteAllLines("${TARGET_PATH}/pwsh-windows-${PS_VERSION}-${VER}.yml", $GitHubActionTemplate, $Utf8NoBomEncoding)
+
+            $TARGET_PATH = "./pwsh/windows/${PS_VERSION}-nanoserver-${VER}"
+            New-Item -Type Directory -Path $TARGET_PATH -Force
+            [System.IO.File]::WriteAllLines("${TARGET_PATH}/Dockerfile", $DockerfileTemplate, $Utf8NoBomEncoding)
         }
     }
 }
